df07db2eda13b1fb4a4175c1b90b5892d7dbd9b9
Ananya Sen <Ananya.Sen@motorola.com>
2012-07-19 17:05:18 -0700

    copy/paste from external apps - removed unnecessary selection change to fix PI redraw issue
    Signed-off-by: Ananya Sen <Ananya.Sen@motorola.com>

--- js/controllers/clipboard-controller.js
+++ js/controllers/clipboard-controller.js
@@ -87,16 +87,6 @@ var ClipboardController = exports.ClipboardController = Montage.create(Component
                 }
             }
 
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
-            }
-
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
-            }
-
             if(this.clipboardContext === "stage"){
                 ElementsClipboardAgent.copy(clipboardEvent);
             }
@@ -114,16 +104,6 @@ var ClipboardController = exports.ClipboardController = Montage.create(Component
                 if(!document.activeElement.getAttribute("data-montage-id") === "menuItemButton") {
                     return;
                 }
-            }
-
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
-            }
-
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
             }
 
             if(this.clipboardContext === "stage"){
@@ -150,16 +130,6 @@ var ClipboardController = exports.ClipboardController = Montage.create(Component
                 if(!document.activeElement.getAttribute("data-montage-id") === "menuItemButton") {
                     return;
                 }
-            }
-
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
-            }
-
-            //perform clipboard operations only if selection tool is selected
-            if(this.application.ninja.toolsData.defaultToolsData[this.application.ninja.toolsData.selectionToolIndex].selected === false){
-                return;
             }
 
             //TODO: return if stage is not focussed
